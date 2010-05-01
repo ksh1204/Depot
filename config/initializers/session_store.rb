7254db7fa231a080a6441476667f813b10303d36
Nick Kim <ksh1204@hotmail.com>
2010-05-01 04:47:37 -0700

    Up to the end of Chapter 8

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,11 +5,11 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_test_session',
+  :key         => '_depot_session',
   :secret      => '6be0e2250f6961a944f622fca15eb8e35dbcc109e65f3e2e9b5e343e229461d8cbad167edab4c25701049ef48a0d1aa88400beecffe2262c1bc99e9d22e1f77d'
 }
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+ActionController::Base.session_store = :active_record_store
